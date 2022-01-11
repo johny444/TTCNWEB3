4ee4d393e35b6fbe2c338cef54c10c86c3784709
Jalajohny PC <johnyjohnny4@gmail.com>
2022-01-11 16:08:24 +0700

    first commit

---
+++ backend-mongodb/src/security/permissions.js
@@ -0,0 +1,150 @@
+const Roles = require('./roles');
+const roles = Roles.values;
+
+class Permissions {
+  static get values() {
+    return {
+      iamEdit: {
+        id: 'iamEdit',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+
+        ],
+        allowedStorageFolders: ['user'],
+      },
+      iamCreate: {
+        id: 'iamCreate',
+        allowedRoles: [
+          roles.librarian,roles.admin
+
+        ],
+      },
+      iamImport: {
+        id: 'iamImport',
+        allowedRoles: [
+          roles.librarian,roles.admin
+
+        ],
+      },
+      iamRead: {
+        id: 'iamRead',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+
+        ],
+      },
+      iamUserAutocomplete: {
+        id: 'iamUserAutocomplete',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+
+        ],
+      },
+      auditLogRead: {
+        id: 'auditLogRead',
+        allowedRoles: [roles.librarian,roles.admin,],
+      },
+      settingsEdit: {
+        id: 'settingsEdit',
+        allowedRoles: [roles.librarian,roles.admin,],
+      },
+      loanImport: {
+        id: 'loanImport',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+        ],
+      },
+      loanCreate: {
+        id: 'loanCreate',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+
+        ],
+        allowedStorageFolders: ['loan'],
+      },
+      loanEdit: {
+        id: 'loanEdit',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+
+        ],
+        allowedStorageFolders: ['loan'],
+      },
+      loanDestroy: {
+        id: 'loanDestroy',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+
+        ],
+        allowedStorageFolders: ['loan'],
+      },
+      loanRead: {
+        id: 'loanRead',
+        allowedRoles: [
+          roles.librarian,roles.Member,roles.admin,
+
+        ],
+      },
+      loanAutocomplete: {
+        id: 'loanAutocomplete',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+
+
+        ],
+      },
+
+      bookImport: {
+        id: 'bookImport',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+
+        ],
+      },
+      bookCreate: {
+        id: 'bookCreate',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+
+        ],
+        allowedStorageFolders: ['book'],
+      },
+      bookEdit: {
+        id: 'bookEdit',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+
+        ],
+        allowedStorageFolders: ['book'],
+      },
+      bookDestroy: {
+        id: 'bookDestroy',
+        allowedRoles: [
+          roles.librarian,roles.admin,
+
+        ],
+        allowedStorageFolders: ['book'],
+      },
+      bookRead: {
+        id: 'bookRead',
+        allowedRoles: [
+          roles.librarian,roles.Member,roles.admin,
+
+        ],
+      },
+      bookAutocomplete: {
+        id: 'bookAutocomplete',
+        allowedRoles: [
+          roles.librarian,roles.Member,roles.admin,],
+      },
+    };
+  }
+
+  static get asArray() {
+    return Object.keys(this.values).map((value) => {
+      return this.values[value];
+    });
+  }
+}
+
+module.exports = Permissions;
---
+++ backend-mongodb/src/security/roles.js
@@ -0,0 +1,11 @@
+class Roles {
+  static get values() {
+    return {
+      librarian: 'Librarian',
+      Member: 'Member',
+      admin: 'Admin',
+    };
+  }
+}
+
+module.exports = Roles;
